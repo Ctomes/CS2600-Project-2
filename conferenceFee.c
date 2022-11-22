9b35f2bfc399821555b8fcb9d763084ab82dcad0
Eugene Kim <eugenekim@cpp.edu>
2022-11-21 19:46:21 -0800

    small function update

--- conferenceFee.c
+++ conferenceFee.c
@@ -11,17 +11,21 @@
 #include "conferenceFee.h"
 
 
-int calculateConferenceExpenses(struct Trip* tripPointer) 
+void calculateConferenceExpenses(struct Trip* tripPointer) 
 {
     // This function will calculate the total conference expenses
     // conference/seminar fee + hotel expenses
 
+    
+
 
 }
 
-double getConferenceFee(struct Trip* tripPointer)
+void getConferenceFee(struct Trip* tripPointer)
 {
-
-    // This function will return the user input of conference fee
-
+    // This function will prompt and return the user input of conference fee
+    double fee; 
+    printf("Please enter conference fee: $"); 
+    scanf("%f", &fee);
+    tripPointer.conferenceFees = fee; 
 }
