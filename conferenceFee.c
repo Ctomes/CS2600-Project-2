5f33541a8d8664b09d6c751ab6b12c8dde85282d
Eugene Kim <eugenekim@cpp.edu>
2022-11-21 18:37:08 -0800

    .c file with note created

---
+++ conferenceFee.c
@@ -0,0 +1,27 @@
+/*
+
+    File name: conferenceFee.c
+    Programmer: Eugene Kim
+
+    This program will calculate the total fee generated from conference. 
+    
+
+*/
+#include <stdio.h>
+#include "conferenceFee.h"
+
+
+int calculateConferenceExpenses(struct Trip* tripPointer) 
+{
+    // This function will calculate the total conference expenses
+    // conference/seminar fee + hotel expenses
+
+
+}
+
+double getConferenceFee(struct Trip* tripPointer)
+{
+
+    // This function will return the user input of conference fee
+
+}
--- conferenceFee.h
+++ conferenceFee.h
@@ -1,1 +1,25 @@
-// This is initial file creation
+// File name: conferenceFee.h
+// Programmer: Eugene Kim
+
+
+typedef struct Trip {
+    int daysSpentOnTrip;
+    float timeOfDeparture;
+    float timeOfArrival;
+    float airfare;
+
+    float rentalCost;
+    int milesDrivenInPrivateCar;
+    float parkingFees;
+    float taxiFees;
+
+    float conferenceFees;
+    float hotelFees;
+    float mealFees;
+
+
+} Trip;
+
+// function prototype list
+int calculateConferenceExpenses(struct Trip trip);
+double getConferenceFee(struct Trip* tripPointer);
