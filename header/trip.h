d2c1838c526f9400d09aa6acd53d1e45b52391b9
Kayla Marie Le <kaylale2014@gmail.com>
2022-11-26 13:09:47 -0800

    missed a couple files

--- header/trip.h
+++ header/trip.h
@@ -1,39 +1,33 @@
 #ifndef TRIP
 #define TRIP
 
-typedef struct Trip {
+typedef struct Trip
+{
     int daysSpentOnTrip;
     float timeOfDeparture;
     float timeOfArrival;
     float airfare;
 
-    float rentalCost;
-    int milesDrivenInPrivateCar;
-    float parkingFees;
-    float parkingFeesAllowable;
-    float taxiFees;
-    float taxiFeesAllowable;
+    float allowedCarCost;
+    float totalCarCost;
 
     float conferenceFees;
     float conferenceFeesAllowable;
     float hotelFees;
     float hotelFeesAllowable;
     float mealFees;
-    float mealFeesAllowable; //never higher than $16 + $16
-
+    float mealFeesAllowable; // never higher than $16 + $16
 
 } Trip;
 
-//Declarations for funcs.
-int calculateFlightExpenses(struct Trip* tripPointer);
-int calculateCarExpenses(struct Trip* tripPointer);
-int calculateConferenceExpenses(struct Trip* tripPointer);
-int calculateMealExpenses(struct Trip* trip);
-void displayToUser(struct Trip* tripPointer);
-//templates: 
-int calculateFlightExpenses(struct Trip* tripPointer){return 0;}
-int calculateCarExpenses(struct Trip* tripPointer){return 0;}
-int calculateConferenceExpenses(struct Trip* tripPointer){return 0;}
-int calculateMealExpenses(struct Trip* tripPointer){return 0;}
-void displayToUser(struct Trip* tripPointer){}
+// Declarations for funcs.
+int calculateFlightExpenses(struct Trip *tripPointer);
+int calculateConferenceExpenses(struct Trip *tripPointer);
+int calculateMealExpenses(struct Trip *trip);
+void displayToUser(struct Trip *tripPointer);
+// templates: [caused multiple definition errors]
+// int calculateFlightExpenses(struct Trip *tripPointer) { return 0; }
+// int calculateConferenceExpenses(struct Trip *tripPointer) { return 0; }
+// int calculateMealExpenses(struct Trip *tripPointer) { return 0; }
+// void displayToUser(struct Trip *tripPointer) {}
 #endif
